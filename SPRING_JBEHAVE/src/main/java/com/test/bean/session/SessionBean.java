c47a3545d26e771e5c7ce946578f21f8d3ae552c
Shalika Weerasinghe <udayangashalika92@gmail.com>
2018-04-11 14:41:00 +0530

    DBconnection and Varlist configuration.

--- SPRING_JBEHAVE/src/main/java/com/test/bean/session/SessionBean.java
+++ SPRING_JBEHAVE/src/main/java/com/test/bean/session/SessionBean.java
@@ -1,8 +1,53 @@
 package com.test.bean.session;
 
-import org.springframework.context.annotation.Scope;
+import java.util.List;
 
-//@Scope("session")
-//public class SessionBean {
-//
-//}
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.test.mapping.useraccount.UserAccount;
+
+@Component
+@Scope("session")
+public class SessionBean {
+	private String sessionId;
+	private String cif;
+	private String userId;
+	private String userName;
+	private List<UserAccount> userAccountList;
+	
+	public String getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	
+	public String getCif() {
+		return cif;
+	}
+	public void setCif(String cif) {
+		this.cif = cif;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public List<UserAccount> getUserAccountList() {
+		return userAccountList;
+	}
+	public void setUserAccountList(List<UserAccount> userAccountList) {
+		this.userAccountList = userAccountList;
+	}
+}
--- SPRING_JBEHAVE/src/main/java/com/test/util/varlist/CommonVarList.java
+++ SPRING_JBEHAVE/src/main/java/com/test/util/varlist/CommonVarList.java
@@ -1,5 +1,17 @@
 package com.test.util.varlist;
 
-public class CommonVarList {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
+public class CommonVarList {
+	
+	@Autowired
+	@Value("${status.default.active}")
+	public String STATUS_DEFAULT_ACTIVE;
+	
+	@Autowired
+	@Value("${status.default.deactive}")
+	public String STATUS_DEFAULT_DEACTIVE;
 }
